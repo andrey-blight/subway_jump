2652af554ef7059b7bbeb1590617495d8317e772
Kizhinov-Andrey <kzihinov05@gmail.com>
2021-12-14 18:20:55 +0300

    Добавлен статический метод запуска игры

--- Scripts/program_class.py
+++ Scripts/program_class.py
@@ -1,4 +1,21 @@
 # Файл в котором лежит главный класс который осуществляет логику игры
-# TODO придумать как назвать (скорее всего будет называтся как и сама игра)
-class NoName:
-    pass
+import pygame
+
+
+class ChristmasJumps:
+    @staticmethod
+    def start_game():
+        """Функция которая запускает основной цикл игры"""
+        pygame.init()
+        screen = pygame.display.set_mode((1000, 600))
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            screen.fill((0, 0, 0))
+            pygame.display.flip()
+        pygame.quit()
+
+
+ChristmasJumps.start_game()
