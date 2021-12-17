66f343e0887d6312569a6373c671160b88226111
Kizhinov-Andrey <kzihinov05@gmail.com>
2021-12-17 12:03:30 +0300

    Изменение констант

--- Scripts/constants.py
+++ Scripts/constants.py
@@ -4,5 +4,5 @@ FPS = 120
 WIDTH = 1920  # Ширина окна
 HEIGHT = 1080  # Высота окна
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))  # Экран
-G = 35 / FPS  # Ускарение свободного падения
-JUMP_HEIGHT = 200  # Высота прыжка
+G = 20 / FPS  # Ускарение свободного падения
+JUMP_HEIGHT = 350  # Высота прыжка
